9f288bb89d245c84beef0f324c165d625796d7f9
samueldemoura <samueldemouramoreira@gmail.com>
2018-05-01 15:30:15 -0300

    Add Gauss elimination test

--- src/numbiosis/backend/Tests/GaussEliminationTest.java
+++ src/numbiosis/backend/Tests/GaussEliminationTest.java
@@ -1,7 +1,8 @@
-package numbiosis.backend;
+package numbiosis.backend.Tests;
 
 import numbiosis.backend.utils.LinearSystem;
 import numbiosis.backend.utils.LinearSystemArray;
+import numbiosis.backend.GaussElimination;
 import numbiosis.backend.utils.Matrix;
 import numbiosis.backend.utils.Vector;
 import org.junit.jupiter.api.Test;
@@ -12,16 +13,34 @@ class GaussEliminationTest {
 
     @Test
     void run() {
-        double[][] matrixArray = {{3, 2, 4},
-                {1, 1, 2},
-                {4, 3, -2}};
-        double[] vectorArray = {1, 2, 3};
+        double[][] matrixArray = {
+                {2, 1, -1},
+                {-3, -1, 2},
+                {-2, 1, 2}};
+        double[] vectorArray = {8, -11, -3};
 
         Matrix matrix = new Matrix(matrixArray);
         Vector vector = new Vector(vectorArray);
         LinearSystem ls = new LinearSystemArray(matrix,vector);
 
         GaussElimination.run(ls);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j)
+                    assertTrue(ls.get(i, j) == 1.0);
+                else
+                    assertTrue(ls.get(i, j) == 0.0);
+
+                System.out.print(ls.get(i, j) + " ");
+            } System.out.print("\n");
+        }
+
+        assertTrue(ls.getVector().get(0) == 2.0);
+        assertTrue(ls.getVector().get(1) == 3.0);
+        assertTrue(ls.getVector().get(2) == -1.0);
+        System.out.println(ls.getVector());
+
 
     }
 }
